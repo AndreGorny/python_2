7caac03ebe67a38762b18a60ebc7a5b71fc0c743
André Gorny <122419632+AndreGorny@users.noreply.github.com>
2023-02-15 21:52:53 -0300

    pasta_aula_2023-02-15

---
+++ 2023-02-15/exercicio_2.py
@@ -0,0 +1,30 @@
+"""Sistema de cadastro:
+    Cliente
+    Funcionário
+    Produto
+
+1) Todos os dados devem ser armazenados em arquivos;
+Cada entidade deve ter seu próprio arquivo.
+
+2) O arquivo deve ter um formato próprio (exemplo.and)
+- Existem bibliotecas que fazem, dentre outras coisas, serialização de classes:
+    Pickle
+
+3) O arquivo deve ser armazenado SEMPRE compactado. Abre o arquivo,
+descompacta,lê (ou escreve), salva, compacta e fecha.
+
+4) Seu sistema deve ter as seguintes opções:
+    a) Inserir novo registro;
+    b) Consultar registro (todos e unitário)
+    c) Deletar registro
+
+Bônus: O sistema deve ter a opção de cadastro de uma nova entidade.
+Ela deve ter seu próprioo arquivo e aparecer no menu inicial
+
+Dicas:
+1º passo: criar as 3 entidades
+2º criar os arquivos
+3º inserir o novo registro
+
+CherryPy - Web Framework
+"""
